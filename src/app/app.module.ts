169761e6829d74958a8b2c0b9740edc5077a7a1a
Lasha <lashiko.deisadze@gmail.com>
2023-09-04 18:56:33 +0400

    project init

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TodoWeekComponent } from './todo-week/todo-week.component';
+import { TodoWeekBoxesComponent } from './todo-week/todo-week-boxes/todo-week-boxes.component';
+import { TodoWeekModalComponent } from './todo-week/todo-week-modal/todo-week-modal.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoWeekComponent,
+    TodoWeekBoxesComponent,
+    TodoWeekModalComponent,
+  ],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
